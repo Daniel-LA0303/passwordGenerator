398ee9ae83d4a6bae8daefe7403f35ebbf9728e6
Daniel-LA0303 <03daniel030399@gmail.com>
2023-01-05 21:07:16 -0600

    add animations in checkbox and show password

--- src/components/PasswordGenerator.jsx
+++ src/components/PasswordGenerator.jsx
@@ -1,5 +1,12 @@
 import React, {useState} from 'react';
 import { motion } from 'framer-motion';
+
+const animations = {
+    initial: {scale: 0},
+    animate: {scale: 1},
+    exit: {scale: 0}
+}
+
 const PasswordGenerator = () => {
 
     const[password, setPassword]=useState('');
@@ -61,7 +68,14 @@ const PasswordGenerator = () => {
             transition={{ duration: 1.5 }}
         >
             <p className=' text-center my-5'>Password Generator</p>
-            <p className=' text-center my-2 h-10 text-xl text-gray-900 font-light'>{password}</p>
+            {password ? 
+                <motion.p 
+                    className=' text-center my-2 h-10 text-xl text-gray-900 font-light'
+                    {...animations} 
+                >{password}</motion.p>
+                : null
+            }
+            
             <div>
                 <div className=' flex justify-center w-full'>
                     <form 
@@ -80,44 +94,60 @@ const PasswordGenerator = () => {
                             />
                         </div>
                         <ul className="w-full mb-5 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-                            <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-                                <div className="flex items-center pl-3">
-                                    <input 
-                                        type="checkbox" 
-                                        onChange={() => setUppercase(!uppercase)}
-                                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" 
-                                    />
-                                    <label className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Include Uppercase Letters</label>
+                            <li className="w-full border-gray-200 rounded-t-lg dark:border-gray-600">
+                                <div className='flex items-center'>
+                                    <label className='px-4'>
+                                        <input 
+                                            type="checkbox"
+                                            onChange={() => setUppercase(!uppercase)}
+                                        />
+                                        <span className="checkbox"></span>
+                                    </label>
+                                    <div className="flex items-center ">
+                                        <label className="w-full py-3 text-sm font-medium text-gray-900 dark:text-gray-300">Include Uppercase Letters</label>
+                                    </div>
                                 </div>
                             </li>
-                            <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-                                <div className="flex items-center pl-3">
-                                    <input 
-                                        type="checkbox"  
-                                        onChange={() => setLowercase(!lowercase)}
-                                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" 
-                                    />
-                                    <label className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Include Lowercase Letters</label>
+                            <li className="w-full border-gray-200 rounded-t-lg dark:border-gray-600">
+                                <div className='flex items-center'>
+                                    <label className='px-4'>
+                                        <input 
+                                            type="checkbox"
+                                            onChange={() => setLowercase(!lowercase)}
+                                        />
+                                        <span className="checkbox"></span>
+                                    </label>
+                                    <div className="flex items-center ">
+                                        <label className="w-full py-3 text-sm font-medium text-gray-900 dark:text-gray-300">Include Lowercase Letters</label>
+                                    </div>
                                 </div>
                             </li>
-                            <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-                                <div className="flex items-center pl-3">
-                                    <input 
-                                        type="checkbox" 
-                                        onChange={() => setNumbers(!numbers)}
-                                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" 
-                                    />
-                                    <label className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Include Numbers</label>
+                            <li className="w-full border-gray-200 rounded-t-lg dark:border-gray-600">
+                                <div className='flex items-center'>
+                                    <label className='px-4'>
+                                        <input 
+                                            type="checkbox"
+                                            onChange={() => setNumbers(!numbers)}
+                                        />
+                                        <span className="checkbox"></span>
+                                    </label>
+                                    <div className="flex items-center ">
+                                        <label className="w-full py-3 text-sm font-medium text-gray-900 dark:text-gray-300">Include Numberss</label>
+                                    </div>
                                 </div>
                             </li>
-                            <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-                                <div className="flex items-center pl-3">
-                                    <input 
-                                        type="checkbox" 
-                                        onChange={() => setSymbols(!symbols)}
-                                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" 
-                                    />
-                                    <label className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Include Symbols</label>
+                            <li className="w-full border-gray-200 rounded-t-lg dark:border-gray-600">
+                                <div className='flex items-center'>
+                                    <label className='px-4'>
+                                        <input 
+                                            type="checkbox"
+                                            onChange={() => setSymbols(!symbols)} 
+                                        />
+                                        <span className="checkbox"></span>
+                                    </label>
+                                    <div className="flex items-center ">
+                                        <label className="w-full py-3 text-sm font-medium text-gray-900 dark:text-gray-300">Include Symbols</label>
+                                    </div>
                                 </div>
                             </li>
                         </ul>
